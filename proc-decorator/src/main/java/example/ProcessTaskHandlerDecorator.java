d5bfd66dc7291b5404dfe58407d9cb6034fc4ae0
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-08-02 23:50:24 +0100

    runtime cdi

--- proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
+++ proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
@@ -1,8 +1,11 @@
 package example;
 
+import javax.inject.Inject;
+
 import org.jbpm.bpmn2.handler.AbstractExceptionHandlingTaskHandler;
 import org.jbpm.process.instance.impl.ProcessInstanceImpl;
 import org.jbpm.workflow.instance.WorkflowProcessInstance;
+import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.api.runtime.process.EventListener;
@@ -10,12 +13,48 @@ import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
+import org.kie.internal.runtime.manager.cdi.qualifier.Singleton;
 import org.kie.internal.runtime.manager.context.ProcessInstanceIdContext;
 
 public class ProcessTaskHandlerDecorator extends AbstractExceptionHandlingTaskHandler {
 
+	@Inject
+	@Singleton
 	private RuntimeManager runtimeManager;
 	private String processId;
+
+	class SubprocessEventListener implements EventListener {
+
+		private long processInstanceId;
+		private WorkItem workItem;
+		private WorkItemManager manager;
+
+		public SubprocessEventListener(long processId, WorkItem workItem, WorkItemManager manager) {
+			super();
+			this.processInstanceId = processId;
+			this.workItem = workItem;
+			this.manager = manager;
+		}
+
+		@Override
+		public void signalEvent(String type, Object event) {
+			System.out.println("ProcessTaskHandlerDecorator.SubprocessEventListener.signalEvent()");
+			if (("processInstanceCompleted:" + processInstanceId).equals(type)) {
+				manager.completeWorkItem(workItem.getId(), null);
+			}
+		}
+
+		@Override
+		public String[] getEventTypes() {
+			System.out.println("ProcessTaskHandlerDecorator.SubprocessEventListener.getEventTypes()");
+			return new String[] { "processInstanceCompleted:" + processInstanceId };
+		}
+
+	}
+
+	public ProcessTaskHandlerDecorator(Class<? extends WorkItemHandler> originalTaskHandlerClass) {
+		super(originalTaskHandlerClass);
+	}
 
 	public ProcessTaskHandlerDecorator(Class<? extends WorkItemHandler> originalTaskHandlerClass,
 			RuntimeManager runtimeManager) {
@@ -39,32 +78,31 @@ public class ProcessTaskHandlerDecorator extends AbstractExceptionHandlingTaskHa
 
 		// Start the subprocess
 		RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
-		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) runtimeEngine.getKieSession()
-				.startProcess(processId);
+		KieSession kieSession = runtimeEngine.getKieSession();
+		ProcessInstance processInstance = (ProcessInstance) kieSession.createProcessInstance(processId,
+				workItem.getParameters());
 
-		((ProcessInstanceImpl) processInstance).setMetaData("ParentProcessInstanceId", workItem.getProcessInstanceId());
+		ProcessInstanceImpl piImpl = (ProcessInstanceImpl) processInstance;
+		piImpl.setMetaData("ParentProcessInstanceId", workItem.getProcessInstanceId());
+		piImpl.setParentProcessInstanceId(workItem.getProcessInstanceId());
+		piImpl.setSignalCompletion(true);
+
+		kieSession.startProcessInstance(processInstance.getId());
 
 		// Attach the event listener
-		EventListener eventListener = new EventListener() {
+		SubprocessEventListener eventListener = new SubprocessEventListener(processInstance.getId(), workItem, manager);
 
-			@Override
-			public void signalEvent(String type, Object event) {
-				if (("processInstanceCompleted:" + processInstance.getId()).equals(type)) {
-					processInstanceCompleted((ProcessInstance) event, workItem, manager);
-				}
-			}
+		if (processInstance.getState() == ProcessInstance.STATE_COMPLETED
+				|| processInstance.getState() == ProcessInstance.STATE_ABORTED) {
+			manager.completeWorkItem(workItem.getId(), null);
+		} else {
+			WorkflowProcessInstance callingPI = (WorkflowProcessInstance) kieSession
+					.getProcessInstance(workItem.getProcessInstanceId());
+			callingPI.addEventListener("processInstanceCompleted:" + processInstance.getId(), eventListener, true);
+//			((WorkflowProcessInstance) processInstance)
+//					.addEventListener("processInstanceCompleted:" + processInstance.getId(), eventListener, true);
 
-			@Override
-			public String[] getEventTypes() {
-				return new String[] { "processInstanceCompleted:" + processInstance.getId() };
-			}
-		};
-		processInstance.addEventListener("processInstanceCompleted:" + processInstance.getId(), eventListener, true);
-
-	}
-
-	protected void processInstanceCompleted(ProcessInstance event, WorkItem workItem, WorkItemManager manager) {
-		manager.completeWorkItem(workItem.getId(), null);
+		}
 	}
 
 	@Override
