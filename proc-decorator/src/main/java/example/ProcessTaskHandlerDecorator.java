37094b982964e9f1a328d0629b7d8d470093b2de
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-08-02 18:37:54 +0100

    start subprocess

--- proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
+++ proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
@@ -1,19 +1,60 @@
 package example;
 
 import org.jbpm.bpmn2.handler.AbstractExceptionHandlingTaskHandler;
+import org.jbpm.process.instance.impl.ProcessInstanceImpl;
+import org.jbpm.workflow.instance.WorkflowProcessInstance;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.process.EventListener;
+import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
+import org.kie.internal.runtime.manager.context.ProcessInstanceIdContext;
 
 public class ProcessTaskHandlerDecorator extends AbstractExceptionHandlingTaskHandler {
 
-	public ProcessTaskHandlerDecorator(Class<? extends WorkItemHandler> originalTaskHandlerClass) {
+	private RuntimeManager runtimeManager;
+	private String processId;
+
+	public ProcessTaskHandlerDecorator(RuntimeManager runtimeManager,
+			Class<? extends WorkItemHandler> originalTaskHandlerClass) {
 		super(originalTaskHandlerClass);
+		this.runtimeManager = runtimeManager;
 	}
 
 	@Override
 	public void handleExecuteException(Throwable cause, WorkItem workItem, WorkItemManager manager) {
 		System.out.println("ProcessTaskHandlerDecorator.handleExecuteException()");
+		
+		// Start the subprocess
+		RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
+		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) runtimeEngine.getKieSession()
+				.startProcess(processId);
+		
+    	((ProcessInstanceImpl) processInstance).setMetaData("ParentProcessInstanceId", workItem.getProcessInstanceId());
+
+		// Attach the event listener
+		EventListener eventListener = new EventListener() {
+
+			@Override
+			public void signalEvent(String type, Object event) {
+				if (("processInstanceCompleted:" + processInstance.getId()).equals(type)) {
+					processInstanceCompleted((ProcessInstance) event, workItem, manager);
+				}
+			}
+
+			@Override
+			public String[] getEventTypes() {
+				return new String[] { "processInstanceCompleted:" + processInstance.getId() };
+			}
+		};
+		processInstance.addEventListener("processInstanceCompleted:" + processInstance.getId(), eventListener, true);
+
+	}
+
+	protected void processInstanceCompleted(ProcessInstance event, WorkItem workItem, WorkItemManager manager) {
+		manager.completeWorkItem(workItem.getId(), null);
 	}
 
 	@Override
