d9b4cd4eb9a812326cd235ec2f92fd07b352d505
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-08-04 20:18:01 +0200

    fixed the event payload, all working

--- proc-decorator/src/main/java/example/RetryHandlingListener.java
+++ proc-decorator/src/main/java/example/RetryHandlingListener.java
@@ -6,6 +6,7 @@ package example;
 import java.util.Map;
 
 import org.jbpm.process.instance.event.SignalManager;
+import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.jbpm.workflow.instance.WorkflowProcessInstance;
 import org.kie.api.runtime.process.EventListener;
 import org.kie.api.runtime.process.WorkItem;
@@ -21,15 +22,14 @@ public class RetryHandlingListener implements EventListener {
 	private WorkItemManager manager;
 	private ProcessTaskHandlerDecorator processTaskHandlerDecorator;
 	private String[] eventTypes;
-	private WorkflowProcessInstance pi;
+	private SignalManager signalManager;
 
 	public RetryHandlingListener(WorkItem workItem, WorkItemManager manager, WorkflowProcessInstance pi, ProcessTaskHandlerDecorator processTaskHandlerDecorator) {
 		this.workItem = workItem;
 		this.manager = manager;
 		this.processTaskHandlerDecorator = processTaskHandlerDecorator;
-		//this.eventTypes = new String[] {"retry:"+pi.getId(),"skip:"+pi.getId()};
+		// this is the event thrown by a process when closing
 		this.eventTypes = new String[] {"processInstanceCompleted:"+pi.getId()};
-	    this.pi = pi;
 	}
 
 	/* (non-Javadoc)
@@ -37,20 +37,34 @@ public class RetryHandlingListener implements EventListener {
 	 */
 	@Override
 	public void signalEvent(String type, Object event) {
-		if (event != null && event instanceof Boolean && ((Boolean)event)) {
-			//retry
-			manager.completeWorkItem(workItem.getId(), null);
-			Map<String, Object> parameters = workItem.getParameters();
-			for (String key : parameters.keySet()) {
-				Object value = pi.getVariable(key);
-				if ( value != null) 
-					parameters.put(key, value);
+		System.out.format("event: %s, payload: %s\n", type, event);
+		
+		if (event instanceof RuleFlowProcessInstance) {
+			RuleFlowProcessInstance processInstance = (RuleFlowProcessInstance) event;
+			Boolean retry = (Boolean) processInstance.getVariable("retry");
+			if (retry!=null && retry == true) {
+				// retry
+				
+				// update the parameters with the values coming from the exception handling process
+				Map<String, Object> parameters = workItem.getParameters();
+				for (String key : parameters.keySet()) {
+					Object value = processInstance.getVariable(key);
+					if ( value != null) 
+						parameters.put(key, value);
+				}
+				
+				// execute again the original WIH
+				processTaskHandlerDecorator.executeWorkItem(workItem, manager);
+			} else {
+				// skip
+				manager.completeWorkItem(workItem.getId(), null);
 			}
-			processTaskHandlerDecorator.executeWorkItem(workItem, manager);
 		} else {
-			//skip
-			manager.completeWorkItem(workItem.getId(), null);
+			System.err.format("event: %s with wrong payload: %s\n", type, event);
 		}
+		
+		// remove this event listner
+		unregister();		
 	}
 
 	/* (non-Javadoc)
@@ -61,9 +75,22 @@ public class RetryHandlingListener implements EventListener {
 		return eventTypes;
 	}
 
+	/*
+	 * register this event listner
+	 */
 	public void register(SignalManager signalManager) {
+		this.signalManager = signalManager;
 		for (String eventType : eventTypes) {
 			signalManager.addEventListener(eventType, this);
+		}
+	}
+
+	/*
+	 * unregister this event listner
+	 */
+	private void unregister() {
+		for (String eventType : eventTypes) {
+			signalManager.removeEventListener(eventType, this);
 		}
 	}
 
