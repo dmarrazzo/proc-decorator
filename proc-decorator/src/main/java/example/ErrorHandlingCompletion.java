35d1a8ed013892bceb107e617e17c4ce1c79eea2
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-08-12 11:05:01 +0200

    Improved internal exception handling

--- proc-decorator/src/main/java/example/ErrorHandlingCompletion.java
+++ proc-decorator/src/main/java/example/ErrorHandlingCompletion.java
@@ -2,16 +2,9 @@ package example;
 
 import java.util.Map;
 
-import org.drools.core.process.instance.impl.WorkItemImpl;
-import org.jbpm.process.core.context.exception.ExceptionScope;
-import org.jbpm.process.instance.context.exception.ExceptionScopeInstance;
 import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
-import org.jbpm.workflow.instance.NodeInstance;
-import org.kie.api.runtime.manager.RuntimeEngine;
-import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
-import org.kie.internal.runtime.manager.context.ProcessInstanceIdContext;
 
 public class ErrorHandlingCompletion extends ProcessCompletionListener {
 
@@ -19,13 +12,9 @@ public class ErrorHandlingCompletion extends ProcessCompletionListener {
 	private WorkItem workItem;
 	private ProcessTaskHandlerDecorator processTaskHandlerDecorator;
 	private Throwable cause;
-	private RuntimeManager runtimeManager;
 
-
-	public ErrorHandlingCompletion(RuntimeManager runtimeManager,
-			ProcessTaskHandlerDecorator processTaskHandlerDecorator, WorkItemManager manager, WorkItem workItem,
-			Throwable cause) {
-		this.runtimeManager = runtimeManager;
+	public ErrorHandlingCompletion(ProcessTaskHandlerDecorator processTaskHandlerDecorator, WorkItemManager manager,
+			WorkItem workItem, Throwable cause) {
 		this.processTaskHandlerDecorator = processTaskHandlerDecorator;
 		this.manager = manager;
 		this.workItem = workItem;
@@ -59,21 +48,14 @@ public class ErrorHandlingCompletion extends ProcessCompletionListener {
 
 	@Override
 	public void processAborted(RuleFlowProcessInstance processInstance) {
-		// retrieve the process instance of the parent process containing the failing service
-		RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
-		RuleFlowProcessInstance parentProcessInstance = (RuleFlowProcessInstance) runtimeEngine.getKieSession().getProcessInstance(workItem.getProcessInstanceId());
+		try {
+			processTaskHandlerDecorator.rethrowException(workItem, cause);
 
-		// retrieve the node instance of the failing servise
-		long nodeInstanceId = ((WorkItemImpl) workItem).getNodeInstanceId();
-		NodeInstance nodeInstance = parentProcessInstance.getNodeInstance(nodeInstanceId, true);
-
-		// throw the exception in the context of the failing services
-		String faultName = cause.getClass().getName();
-		ExceptionScopeInstance contextInstance = (ExceptionScopeInstance) nodeInstance.resolveContextInstance(ExceptionScope.EXCEPTION_SCOPE, faultName);	
-		contextInstance.handleException(faultName, cause);
-		
-		// mark the work item failed and call the abort procedure
-		manager.abortWorkItem(workItem.getId());
+			// mark the work item failed and call the abort procedure
+			manager.abortWorkItem(workItem.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
+++ proc-decorator/src/main/java/example/ProcessTaskHandlerDecorator.java
@@ -1,9 +1,13 @@
 package example;
 
+import org.drools.core.process.instance.impl.WorkItemImpl;
 import org.jbpm.bpmn2.handler.AbstractExceptionHandlingTaskHandler;
+import org.jbpm.process.core.context.exception.ExceptionScope;
 import org.jbpm.process.instance.ProcessInstance;
+import org.jbpm.process.instance.context.exception.ExceptionScopeInstance;
 import org.jbpm.process.workitem.rest.RESTWorkItemHandler;
 import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
+import org.jbpm.workflow.instance.NodeInstance;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
@@ -39,37 +43,70 @@ public class ProcessTaskHandlerDecorator extends AbstractExceptionHandlingTaskHa
 	public void handleExecuteException(Throwable cause, WorkItem workItem, WorkItemManager manager) {
 		System.out.println("ProcessTaskHandlerDecorator.handleExecuteException()");
 
-		if (processId == null)
-			processId = (String) workItem.getParameter("processId");
+		try {
+			if (processId == null)
+				processId = (String) workItem.getParameter("processId");
 
-		// Create the kiesession
-		RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
-		KieSession kieSession = runtimeEngine.getKieSession();
+			// Create the kiesession
+			RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
+			KieSession kieSession = runtimeEngine.getKieSession();
 
-		// Create the subprocess
-		RuleFlowProcessInstance processInstance = (RuleFlowProcessInstance) kieSession.createProcessInstance(processId,
-				workItem.getParameters());
+			// Create the subprocess
+			RuleFlowProcessInstance processInstance = (RuleFlowProcessInstance) kieSession
+					.createProcessInstance(processId, workItem.getParameters());
 
-		long parentInstanceId = workItem.getProcessInstanceId();
-		processInstance.setMetaData("ParentProcessInstanceId", parentInstanceId);
-		processInstance.setParentProcessInstanceId(parentInstanceId);
+			long parentInstanceId = workItem.getProcessInstanceId();
+			processInstance.setMetaData("ParentProcessInstanceId", parentInstanceId);
+			processInstance.setParentProcessInstanceId(parentInstanceId);
 
-		// Start the subprocess
-		kieSession.startProcessInstance(processInstance.getId());
+			// Start the subprocess
+			kieSession.startProcessInstance(processInstance.getId());
 
-		// Manage the subprocess completion
-		if (processInstance.getState() == ProcessInstance.STATE_COMPLETED
-				|| processInstance.getState() == ProcessInstance.STATE_ABORTED) {
-			manager.completeWorkItem(workItem.getId(), null);
-		} else {
-			ErrorHandlingCompletion errorHandlingCompletion = new ErrorHandlingCompletion(runtimeManager, this, manager, workItem, cause);
-			errorHandlingCompletion.listenTo(processInstance);
+			// Manage the subprocess completion
+			if (processInstance.getState() == ProcessInstance.STATE_COMPLETED
+					|| processInstance.getState() == ProcessInstance.STATE_ABORTED) {
+				manager.completeWorkItem(workItem.getId(), null);
+			} else {
+				ErrorHandlingCompletion errorHandlingCompletion = new ErrorHandlingCompletion(this, manager, workItem,
+						cause);
+				errorHandlingCompletion.listenTo(processInstance);
+			}
+		} catch (Exception e) {
+			System.err.println("Error in " + this.getClass().getName() + " caused by: " + e
+					+ "rethrowing the original cause to the originator process instance");
+			try {
+				rethrowException(workItem, cause);
+			} catch (Exception rethrowException) {
+				rethrowException.printStackTrace();
+			}
 		}
 	}
 
 	@Override
 	public void handleAbortException(Throwable cause, WorkItem workItem, WorkItemManager manager) {
 		System.err.println("ProcessTaskHandlerDecorator.handleAbortException()");
+	}
+
+	public void rethrowException(WorkItem workItem, Throwable cause) throws Exception {
+		// retrieve the process instance of the parent process containing the failing
+		// service
+		RuntimeEngine runtimeEngine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
+		RuleFlowProcessInstance parentProcessInstance = (RuleFlowProcessInstance) runtimeEngine.getKieSession()
+				.getProcessInstance(workItem.getProcessInstanceId());
+
+		// retrieve the node instance of the failing servise
+		long nodeInstanceId = ((WorkItemImpl) workItem).getNodeInstanceId();
+		NodeInstance nodeInstance = parentProcessInstance.getNodeInstance(nodeInstanceId, true);
+
+		// throw the exception in the context of the failing services
+		String faultName = cause.getClass().getName();
+		ExceptionScopeInstance exceptionScopeInstance = (ExceptionScopeInstance) nodeInstance
+				.resolveContextInstance(ExceptionScope.EXCEPTION_SCOPE, faultName);
+
+		if (exceptionScopeInstance == null)
+			throw new Exception("Exception Scope not found in process: " + parentProcessInstance.getProcessId());
+
+		exceptionScopeInstance.handleException(faultName, cause);
 	}
 
 }
